519c9fba2d0454abdbbac03a954f067390a78d44
Piet Lammers <piet@Piets-MacBook-Pro.local>
2020-04-28 16:10:50 +0100

    Add functionality for drawing paths

---
+++ pytikz/constants.py
@@ -0,0 +1,14 @@
+
+# shorthands for line widths in tikz
+LINE_WIDTHS = [
+    'ultra thin',
+    'very thin',
+    'thin',
+    'semithick',
+    'thick',
+    'very thick',
+    'ultra thick'
+]
+
+# default fill color for this package
+DEFAULT_FILL_COLOR = 'yellow'
