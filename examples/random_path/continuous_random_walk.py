16a00e74c29f6e8a07062285f2af8826cafcd510
Onno Kampman <onno.kampman@gmail.com>
2020-05-15 12:54:01 +0100

    add continuous random walk figure

---
+++ examples/random_path/continuous_random_walk.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+
+from pytikz.latex_figure import LatexFigure
+from pytikz.path import DrawablePath
+
+
+if __name__ == "__main__":
+    fig = LatexFigure('figure', os.path.dirname(__file__))
+
+    figure_width = 40
+
+    n_steps = 3000
+    vertices = np.vstack(
+        (np.zeros(n_steps), np.arange(n_steps) / n_steps * figure_width)
+    ).T
+    a = 0
+    for i in range(n_steps):
+        a += np.random.randn() / np.sqrt(n_steps)
+        vertices[i, 0] = a
+    print(vertices)
+    # vertices /= np.sqrt(n_steps)
+    # vertices = 10 * np.random.rand(10, 2)
+
+    path = DrawablePath(vertices)
+    path.line_join = 'round'
+    fig.draw(path)
+
+    fig.update(hard=True)
