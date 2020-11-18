58f419d102c2b6ac73655d627baf39bafdc21f4c
pglammers <email@pietlammers.com>
2020-11-18 17:11:44 +0100

    delete obsolete drawing classes

--- pytikz/__init__.py
+++ pytikz/__init__.py
@@ -1,7 +1,3 @@
 from .latex_figure import LatexFigure
-# from .shape.path import DrawablePath
-# from .shape.circle import Circle
-# from .shape.grid import Grid
-# from .shape.rectangle import Rectangle
-from .path import *
 from .constants import *
+from .path import *
