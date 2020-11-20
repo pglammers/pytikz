56fa5a34a83580c9252b444ffcb0e75cb27a552f
pglammers <email@pietlammers.com>
2020-11-20 15:05:51 +0100

    drawable

--- pytikz/__init__.py
+++ pytikz/__init__.py
@@ -1,4 +1,5 @@
 from .latex_figure import LatexFigure
 from .vector import Vector
 from .path import Path
+from .drawable import Drawable
 from .constants import *
---
+++ pytikz/drawable.py
@@ -0,0 +1,29 @@
+class Drawable:
+	line = True
+	line_color = None
+	line_width = None
+	line_join = None
+
+	fill = False
+	fill_color = None
+
+	def __init__(self, shape):
+		self.shape = shape
+
+	def __str__(self):
+		if self.fill: assert self.shape.cycle
+
+		if not self.line:
+			if self.fill: return f"\\fill[{self.fill_color}] {self.shape};"
+			if not self.fill: return ""
+
+		if self.line:
+
+			options = []
+			if self.line_color: options.append(self.line_color)
+			if self.line_width: options.append(self.line_width.value)
+			if self.line_join: options.append(f"line join={self.line_join.value}")
+			if self.fill: options.append(f"fill={self.fill_color}")
+			options = f"[{', '.join(options)}]" if options else ""
+
+			return f"\\draw{options} {self.shape};"
