fead13c31bb39c72f1e445dcddc117b9aab2d372
pglammers <email@pietlammers.com>
2020-11-23 13:26:36 +0100

    update tests

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -1,15 +1,27 @@
 import pytest
 
 from pytikz import Vector
-import numpy as np
+
+import numpy
 
 
 def test_vector():
-	a = Vector(1, 2, 3)
-	ar = np.array([
+
+	# Test if Vectors are cast to the correct shape under all circumstances
+	assert Vector(    ).shape == (0, )
+	assert Vector(0   ).shape == (1, )
+	assert Vector(0, 0).shape == (2, )
+
+	# Test the string representation of a vector
+	assert str(Vector(1, 2, 3)) == "(1, 2, 3)"
+
+	# Test the behaviour of Vectors under linear transformations
+	transformation = numpy.array([
 		[1, 0],
 		[3, 1],
 		[4, 0]
 	])
-	assert str(Vector(1, 2, 3)) == "(1, 2, 3)"
-	assert np.all(a @ ar == Vector(19, 2))
+	assert numpy.all(Vector(1, 2, 3) @ transformation == Vector(19, 2))
+
+	# Test the string representation of a matrix
+	assert str(numpy.ones((2, 2), dtype=numpy.int8)) == "(1, 1) -- (1, 1)"
