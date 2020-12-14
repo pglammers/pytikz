63b8a4f229c2be10f171cdc7b1af9916b6c5fadf
pglammers <email@pietlammers.com>
2020-12-14 11:32:54 +0100

    update imports

--- pytikz/__init__.py
+++ pytikz/__init__.py
@@ -2,5 +2,5 @@ from .latex_figure import LatexFigure
 from .vector import Vector
 from .shape import Path
 from .drawable import Drawable, Drawer
-from .view import View, IdentityProjection, ScaleProjection
+from .view import View
 from .constants import *
