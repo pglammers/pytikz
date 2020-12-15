84ddbf79558356c4504456b9c6462cc329667992
pglammers <email@pietlammers.com>
2020-12-15 13:06:09 +0100

    unit test drawable

--- tests/test_drawable.py
+++ tests/test_drawable.py
@@ -1,37 +1,34 @@
 import pytest
 
-from pytikz import Vector, Path, Drawable, LineWidth, LineJoin
+from pytikz import Vector, Path, Drawable, Drawer, LineWidth, LineJoin
 
 
 def test_drawable():
     p = Path([Vector(0), Vector(1)])
-    d = Drawable(p)
+    l = Drawer()
+    d = Drawable(l, p)
     assert str(d) == "\\draw (0) -- (1);"
-    d.fill = True
-    d.fill_color = "blue"
+
+    l.fill = True
+    l.fill_color = "blue"
     with pytest.raises(AssertionError):
         str(d)
 
-    p = Path([Vector(0)], cycle=True)
-    d = Drawable(p)
+    p.cycle = True
+    assert str(d) == "\\draw[fill=blue] (0) -- (1) -- cycle;"
 
-    assert str(d) == "\\draw (0) -- cycle;"
+    l.line = False
+    assert str(d) == "\\fill[blue] (0) -- (1) -- cycle;"
 
-    d.fill = True
-    d.fill_color = "blue"
-    assert str(d) == "\\draw[fill=blue] (0) -- cycle;"
+    l.line = True
+    l.line_color = "blue"
+    assert str(d) == "\\draw[blue, fill=blue] (0) -- (1) -- cycle;"
 
-    d.line = False
-    assert str(d) == "\\fill[blue] (0) -- cycle;"
+    l.line_width = LineWidth.ULTRA_THIN
+    assert str(d) == "\\draw[blue, ultra thin, fill=blue] (0) -- (1) -- cycle;"
 
-    d.line = True
-    d.line_color = "blue"
-    assert str(d) == "\\draw[blue, fill=blue] (0) -- cycle;"
-
-    d.line_width = LineWidth.ULTRA_THIN
-    assert str(d) == "\\draw[blue, ultra thin, fill=blue] (0) -- cycle;"
-
-    d.line_join = LineJoin.MITER
+    l.line_join = LineJoin.MITER
     assert (
-        str(d) == "\\draw[blue, ultra thin, line join=miter, fill=blue] (0) -- cycle;"
+        str(d)
+        == "\\draw[blue, ultra thin, line join=miter, fill=blue] (0) -- (1) -- cycle;"
     )
