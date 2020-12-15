10577858d9f38358acc887408b78aa874f76a3b9
pglammers <email@pietlammers.com>
2020-12-15 13:32:50 +0100

    update example

--- examples/random_path/main.py
+++ examples/random_path/main.py
@@ -1,18 +1,20 @@
 import os
 import numpy as np
 
-from pytikz import LatexFigure
-from pytikz import DrawablePath
+from pytikz import LatexFigure, Vector, Path, Drawer, Drawable, LineJoin
 
 
 if __name__ == "__main__":
     fig = LatexFigure("figure", os.path.dirname(__file__))
 
-    fig.append_string("\clip (0,0) rectangle (15,15);\n")
+    data = 5 * np.random.rand(20, 2)
+    vectors = [Vector(*a) for a in data]
+    path = Path(vectors)
 
-    vertices = 15 * np.random.rand(20, 2)
-    path = DrawablePath(vertices)
-    path.line_join = "round"
-    fig.draw(path)
+    line = Drawer()
+    line.line_join = LineJoin.ROUND
 
+    d = Drawable(line, path)
+
+    fig.draw(d)
     fig.build()
