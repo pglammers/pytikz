0e1102360e53ba2bca35d4eca48e55ad3aed409f
pglammers <email@pietlammers.com>
2021-01-02 12:57:31 +0100

    implement circle shape

--- pytikz/shape.py
+++ pytikz/shape.py
@@ -36,6 +36,12 @@ class Shape(ABC):
             self.anchor = transformation(self.anchor)
         return self
 
+    def _view(self, item):
+        if self.anchor is None:
+            return item
+        else:
+            return self.anchor + item
+
 
 class Path(Shape, AbstractList):
     """Class for describing straight lines connecting a sequence of vectors."""
@@ -48,12 +54,6 @@ class Path(Shape, AbstractList):
             self.append(vector)
         self.cycle = cycle
         self.anchor = anchor
-
-    def _view(self, item):
-        if self.anchor is None:
-            return item
-        else:
-            return self.anchor + item
 
     @classmethod
     def rectangle(self, left, right, lower, upper, anchor=None):
@@ -77,3 +77,26 @@ class Path(Shape, AbstractList):
     def apply_internally(self, transformation):
         """Applies the transformation to the vectors in .vector_list."""
         self._list = [transformation(v) for v in self._list]
+
+
+class Circle(Shape):
+
+    cycle = True
+
+    def __init__(self, center, radius, anchor=None):
+        self._center = center
+        self.radius = radius
+        self.anchor = anchor
+
+    @property
+    def center(self):
+        return self._view(self._center)
+
+    def __str__(self):
+        return f"{str(self.center)} circle ({self.radius})"
+
+    def copy(self):
+        return Circle(self._center, self.radius, self.anchor)
+
+    def apply_internally(self, transformation):
+        self._center = transformation(self._center)
