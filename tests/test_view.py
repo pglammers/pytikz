ca034fddb48ea35f2443f053fb7d00e69fa43853
pglammers <email@pietlammers.com>
2021-01-05 20:01:55 +0100

    update view test

--- tests/test_view.py
+++ tests/test_view.py
@@ -2,15 +2,14 @@ import pytikz as pt
 import pytest
 
 
-# def test_view():
-#     a, b = pt.Vector(1), pt.Vector(2)
-#     p1 = pt.Path([a, b])
-#     p2 = pt.Path([a, b], anchor=a)
-#     d = pt.ShapeStyle()
-#     o1 = pt.DrawableShape(p1, d)
-#     o2 = pt.DrawableShape(p2, d)
-#     v = pt.View()
-#     v.append(o1)
-#     v.append(o2)
-#     assert str(v[0]) == str(o1) == "\\draw (1) -- (2);"
-#     assert str(v[1]) == str(o2) == "\\draw (2) -- (3);"
+def test_view():
+    a, b = pt.Vector(1), pt.Vector(2)
+    p1 = pt.Path([a, b])
+    p2 = pt.Path([a, b])
+    d = pt.ShapeStyle()
+    o1 = d(p1)
+    o2 = d(p2)
+    v = pt.View(pt.vector.Transformation(lambda x:x+a))
+    v.append(o1)
+    v.append(o2)
+    assert str(v[0]) == "\\draw (2) -- (3);"
