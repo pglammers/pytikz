85fb82bc78f1eedf9bf29a1aea746e9b24a249a4
pglammers <email@pietlammers.com>
2021-01-12 16:17:09 +0100

    docstrings vector.py

--- pytikz/vector.py
+++ pytikz/vector.py
@@ -2,11 +2,19 @@ from abc import ABC, abstractmethod
 import numpy as np
 
 
-# Implementation of Vector and of the string representation of Vector
-
-
 def _tikz_representation(array):
-    """Alternative .__str__ method for np.ndarray."""
+    """Returns the pgf/tikz string representation of the array.
+
+    Args:
+        array (np.ndarray): The array which requires a string representation.
+
+    Returns:
+        str: The pgf/tikz string representation.
+
+    Raises:
+        ValueError: If the dimension of the array is not 1 or 2.
+
+    """
     if array.ndim == 1:
         return f"({', '.join(str(c) for c in array)})"
     elif array.ndim == 2:
@@ -19,24 +27,39 @@ def _tikz_representation(array):
 
 np.set_string_function(
     _tikz_representation, False
-)  # Presents this .__str__ method to np.
+)  # Presents this .__str__ method to numpy.
 
 
-VectorType = type(np.array([]))
+VectorType = type(np.array([]))  # Vectors have type np.ndarray.
 
 
 class VectorLike(ABC):
+    """Abstract class for instances that can be cast to VectorType."""
+
     @property
     @abstractmethod
     def vector(self):
+        """Returns the VectorType representation of the instance."""
         pass
 
     def __str__(self):
+        """Returns the pgf/tikz string representation of the instance."""
         return str(self.vector)
 
 
 def Vector(*args):
-    """Generates a vector; used to control its generation."""
+    """Generates a VectorType instance.
+
+    Args:
+        *args: Variable length arguments list, of one of the following types.
+            [VectorType]: A single VectorType instance.
+            [VectorLike]: A single VectorLike instance.
+            [x, y, ...]: A variable number of real numbers.
+
+    Returns:
+        np.ndarray: The VectorType representation of the vector.
+
+    """
     args = list(args)
     if len(args) == 1 and isinstance(args[0], VectorType):
         return args[0]
@@ -46,28 +69,50 @@ def Vector(*args):
         return np.array(args)
 
 
-# Implementation of transformations
-
-
 class Shiftable(ABC):
+    """Abstract class for instances that may be subjected to Shift instances."""
+
     @abstractmethod
     def copy(self):
+        """Returns a copy of the instance."""
         pass
 
     @abstractmethod
     def apply(self, transformation):
+        """Applies the transformation internally to the instance.
+
+        Args:
+            transformation (Transformation): The transformation that should act on the instance.
+
+        This method must return None.
+
+        """
         pass
 
 
 class Scalable(Shiftable):
+    """Abstract class for instances that may be subjected to Scaling instances."""
+
     pass
 
 
 class Transformable(Scalable):
+    """Abstract class for instances that may be subjected to Transformation instances."""
+
     pass
 
 
 class Transformation:
+    """Wrapper class for functions that act on Transformable instances.
+
+    Args:
+        transformation (function): The wrapped function.
+
+    Attributes:
+        _transformation (function): The wrapped function.
+        _subject_type (type): The type of arguments that may be passed to the wrapped function, other than VectorType.
+
+    """
 
     _subject_type = Transformable
 
@@ -75,6 +120,23 @@ class Transformation:
         self._transformation = transformation
 
     def __call__(self, subject, inplace=False):
+        """Applies ._transformation to subject.
+
+        Args:
+            subject (_subject_type or VectorType): The subject that is passed.
+            inplace (bool): If the subject should be copied before application.
+
+        Returns:
+            _subject_type or VectorType: The transformed subject.
+
+        Raises:
+            ValueError: If subject is not of type VectorType or of type _subject_type.
+
+        If subject is of VectorType, the transformation is applied directly to the argument.
+        Otherwise, the method calls subject.apply(self), and the subject handles the further internal application of the transformation.
+        If inplace is False, then the subject is copied before application.
+
+        """
         if isinstance(subject, VectorType):
             return self._transformation(subject)
         elif not isinstance(subject, self._subject_type):
@@ -87,6 +149,16 @@ class Transformation:
 
 
 class Scaling(Transformation):
+    """Wrapper class for functions that act on Scalable instances.
+
+    A Scaling is a specific type of Transformation, and applies to a wider range of instances.
+
+    Args:
+        x (float): The horizontal scaling factor.
+        y (float): The vertical scaling factor.
+        origin (VectorType): The origin that should be shifted to (0, 0) before scaling.
+
+    """
 
     _subject_type = Scalable
 
@@ -102,6 +174,14 @@ class Scaling(Transformation):
 
 
 class Shift(Scaling):
+    """Wrapper class for functions that act on Shiftable instances.
+
+    A Shift is a specific type of Scaling, and applies to a wider range of instances.
+
+    Args:
+        origin (VectorType): The origin that should be shifted to (0, 0).
+
+    """
 
     _subject_type = Shiftable
 
@@ -112,19 +192,32 @@ class Shift(Scaling):
 
 
 def compose(a, b):
+    """A simple function for composing two Transformations.
+
+    Args:
+        a (Transformation): The Transformation that should be applied last.
+        b (Transformation): The Transformation that should be applied first.
+
+    Returns:
+        function: The composition of the two function calls.
+
+    """
     return lambda v: a(b(v))
 
 
-# Definition of AnchoredObject and implementation of AnchoredVector
-
-
 class AnchoredObject(ABC):
-    """Implements the .anchor property."""
+    """Abstract class for anchored instances.
+
+    When a transformation is applied to an AnchoredObject, the Transformation is only applied to the anchor attribute.
+
+    Attributes:
+        anchor (VectorType or VectorLike): The anchor that the Transformation should work on.
+
+    """
 
     anchor = None
 
     def apply(self, transformation):
-        """Distributes the application of the transformation."""
         if self.anchor is None:
             self.apply_internally(transformation)
         else:
@@ -132,6 +225,15 @@ class AnchoredObject(ABC):
         return self
 
     def anchor_resolve(self, vector):
+        """Returns the position of a vector relative to the anchor.
+
+        Args:
+            vector (VectorType or VectorLike): The relative vector to be resolved.
+
+        Returns:
+            VectorType: The absolute position of the resolved vector.
+
+        """
         if self.anchor is None:
             return Vector(vector)
         else:
@@ -139,6 +241,18 @@ class AnchoredObject(ABC):
 
 
 class AnchoredVector(VectorLike, AnchoredObject, Transformable):
+    """VectorLike instance which consists of two vector: an absolute and a relative vector.
+
+    When a Transformation is applied to an AnchoredVector, it is only applied to the anchor.
+    The offset attribute remains unchanged.
+    When an AnchoredVector is cast to VectorType, the two vectors are summed.
+
+    Args:
+        anchor (VectorType or VectorLike): The transformable anchor vector.
+        offset (VectorType or VectorLike): The offset vector that is not transformed.
+
+    """
+
     def __init__(self, anchor, offset):
         self.anchor = anchor
         self.offset = offset
