d8c84726a0893df717fbd6b3867b81814efa9849
pglammers <email@pietlammers.com>
2021-01-13 13:49:50 +0100

    view docstrings

--- pytikz/view.py
+++ pytikz/view.py
@@ -24,22 +24,49 @@ class View(Drawable, Transformable, AbstractList):
         self.clip = clip
 
     def _view(self, item):
-        """Returns the transformed item."""
+        """Implements the _view method from AbstractList.
+
+        Args:
+            item (Drawable): The item to be viewed.
+
+        Returns:
+            Drawable: The transformed item.
+
+        """
         return self.transformation(item)
 
     def __str__(self):
-        """Returns a string with the transformed Drawables concatenated, and clipped whenever a ClosedShape is provided."""
+        """Implements the __str__ method from Drawable.
+
+        Returns a string with the transformed Drawables concatenated, and clipped whenever a ClosedShape is provided.
+
+        Returns:
+            str: The string pgf/tikz string representation of the view.
+
+        """
         data = "\n".join(str(d) for d in self)
         return data if self.clip is None else self.clip.clip(data)
 
     def copy(self):
-        """Returns a copy of the View, where the list is passed as a reference."""
+        """Implements the copy method from Shiftable.
+
+        Returns:
+            View: A copy of the View, where the list is passed as a reference.
+
+        """
         view = View(self.transformation, self.clip)
         view._list = self._list
         return view
 
     def apply(self, transformation):
-        """Applies the transformation to the View, by composing it with the internal transformation, and applying it to the ClosedShape."""
+        """Implements the apply method from Shiftable.
+
+        Applies the transformation to the View, by composing it with the internal transformation, and applying it to the ClosedShape.
+
+        Args:
+            transformation (Transformation or function): The transformation to be applied.
+
+        """
         self.transformation = compose(transformation, self.transformation)
         if self.clip is not None:
             self.clip = transformation(self.clip)
