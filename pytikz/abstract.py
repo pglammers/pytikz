bc56cf9554149f1ec190c7205c70ee03a9dec240
pglammers <email@pietlammers.com>
2021-01-13 08:37:00 +0100

    docstrings abstract

--- pytikz/abstract.py
+++ pytikz/abstract.py
@@ -1,17 +1,29 @@
 from abc import ABC, abstractmethod
 
 
-# Definition of Drawable
-
-
 class Drawable(ABC):
+    """Abstract Drawable class for instances that can be drawn onto the pgf/tikz canvas.
+
+    The string representation of the instance should be self-contained.
+
+    """
+
     @abstractmethod
     def __str__(self):
+        """Must return the representation of the Drawable as a pgf/tikz string."""
         pass
 
 
 class AbstractList(ABC):
-    """Wrapped list which transforms elements through the .view method upon access."""
+    """An AbstractList is a wrapped list which transforms elements through the .view method upon access.
+
+    Args:
+        data (list): The wrapped list.
+
+    Attributes:
+        _list (list): The wrapped list.
+
+    """
 
     _list = None
 
@@ -21,17 +33,22 @@ class AbstractList(ABC):
             self._list = data
 
     def _view(self, item):
+        """The _view method equals the identity transformation by default."""
         return item
 
     def append(self, item):
+        """Appends the item to the wrapped list."""
         self._list.append(item)
 
     def __setitem__(self, k, value):
+        """Sets the k-th entry of the wrapped list to the value provided."""
         self._list[k] = value
 
     def __getitem__(self, k):
+        """Retrieves the transformed version of the k-th entry of the wrapped list."""
         return self._view(self._list[k])
 
     def __iter__(self):
+        """Iterates over the transformed entries of the wrapped list."""
         for k in range(len(self._list)):
             yield self[k]
