360d05152ad0c58aef89daf8188a1256cf0e3476
Piet Lammers <email@pietlammers.com>
2021-12-03 16:50:14 -0500

    Node test

--- tests/test_node.py
+++ tests/test_node.py
@@ -5,23 +5,23 @@ import numpy as np
 
 def test_node():
     n = pt.Node(pt.Vector(1, 1), "test")
-    assert str(n) == "\\node at (1, 1) {test};"
+    assert pt.object_string(n) == "\\node at (1, 1) {test};"
 
     m = pt.Node(pt.vector.AnchoredVector(pt.Vector(3, 3), pt.Vector(5, 5)), "test")
-    assert str(m) == "\\node at (8, 8) {test};"
+    assert pt.object_string(m) == "\\node at (8, 8) {test};"
 
     t = pt.vector.Transformation(lambda x: 2 * x)
-    assert str(t(n)) == "\\node at (2, 2) {test};"
-    assert str(t(m)) == "\\node at (11, 11) {test};"
+    assert pt.object_string(t(n)) == "\\node at (2, 2) {test};"
+    assert pt.object_string(t(m)) == "\\node at (11, 11) {test};"
 
-    assert str(n) == "\\node at (1, 1) {test};"
-    assert str(m) == "\\node at (8, 8) {test};"
+    assert pt.object_string(n) == "\\node at (1, 1) {test};"
+    assert pt.object_string(m) == "\\node at (8, 8) {test};"
 
     t(n, inplace=True)
     t(m, inplace=True)
 
-    assert str(n) == "\\node at (2, 2) {test};"
-    assert str(m) == "\\node at (11, 11) {test};"
+    assert pt.object_string(n) == "\\node at (2, 2) {test};"
+    assert pt.object_string(m) == "\\node at (11, 11) {test};"
 
     n = pt.Node(pt.Vector(1, 1), "test", pt.Orientation.SOUTH_EAST)
     assert pt.object_string(n) == "\\node[anchor=south east] at (1, 1) {test};"
